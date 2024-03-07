28f8ba18b16e9d6715d70c1a5b24d3f7c6c9e785
STRYKER316 <ansil.mishra316@gmail.com>
2024-03-07 23:23:37 +0530

    Register User model in core app & Define custom admin page for User model

--- app/core/admin.py
+++ app/core/admin.py
@@ -1,3 +1,16 @@
-from django.contrib import admin    # noqa: F401
+""" Django admin customization """
+
+from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin as BaseUserAdmin
+
+from core import models
+
+
+class CustomUserAdmin(BaseUserAdmin):
+    """ Define admin page for user model """
+    ordering = ['id']
+    list_display = ['email', 'name']
+
 
 # Register your models here.
+admin.site.register(models.User, CustomUserAdmin)
