f34ae0cd2e199321b9be54bb4860df1bc7f6f39a
STRYKER316 <ansil.mishra316@gmail.com>
2024-03-07 23:50:02 +0530

    Refactor admin.py to show change user page on admin panel and add unit-test for edit_user_page

--- app/core/admin.py
+++ app/core/admin.py
@@ -1,15 +1,32 @@
 """ Django admin customization """
 
 from django.contrib import admin
-from django.contrib.auth.admin import UserAdmin as BaseUserAdmin
+from django.contrib.auth.admin import UserAdmin
+from django.utils.translation import gettext_lazy as translate
 
 from core import models
 
 
-class CustomUserAdmin(BaseUserAdmin):
+class CustomUserAdmin(UserAdmin):
     """ Define admin page for user model """
     ordering = ['id']
     list_display = ['email', 'name']
+
+    fieldsets = (
+        (None, {'fields': ('email', 'password')}),
+        (
+            translate('Permissions'),
+            {
+                'fields': (
+                    'is_active',
+                    'is_staff',
+                    'is_superuser'
+                )
+            }
+        ),
+        (translate('Important dates'), {'fields': ('last_login',)}),
+    )
+    readonly_fields = ['last_login']
 
 
 # Register your models here.
