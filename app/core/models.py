0d415a98a94dcda7f99b90742f4135323a5083ac
STRYKER316 <ansil.mishra316@gmail.com>
2024-03-07 22:51:18 +0530

    Add create_superuser method to UserManager, unit-test case for creating a new superuser

--- app/core/models.py
+++ app/core/models.py
@@ -22,6 +22,14 @@ class UserManager(BaseUserManager):
             **extra_fields)
         user.set_password(password)
         user.save(using=self._db)
+        return user
+
+    def create_superuser(self, email, password):
+        """ Create, save, and return a new superuser """
+        user = self.create_user(email, password)
+        user.is_staff = True
+        user.is_superuser = True
+        user.save(using=self._db)
 
         return user
 
--- app/core/tests/test_models.py
+++ app/core/tests/test_models.py
@@ -20,7 +20,6 @@ class ModelTests(TestCase):
         self.assertEqual(user.email, email)
         self.assertTrue(user.check_password(password))
 
-
     def test_new_user_email_normalized(self):
         """ Test the email for a new user is normalized """
         sample_emails = [
@@ -35,8 +34,17 @@ class ModelTests(TestCase):
 
             self.assertEqual(user.email, expected_email)
 
-
     def test_new_user_without_email_raise_error(self):
         """ Test creating user without email raises error """
         with self.assertRaises(ValueError):
             get_user_model().objects.create_user(None, 'sample123')
+
+    def test_create_new_superuser(self):
+        """ Test creating a new superuser """
+        user = get_user_model().objects.create_superuser(
+            'test@example.com',
+            'test123'
+        )
+
+        self.assertTrue(user.is_superuser)
+        self.assertTrue(user.is_staff)
