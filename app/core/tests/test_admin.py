8b0a113b6d47db392cd51c5b01222e7da24c513a
STRYKER316 <ansil.mishra316@gmail.com>
2024-03-07 23:15:27 +0530

    Add unit-test for Django admin page user listing

---
+++ app/core/tests/test_admin.py
@@ -0,0 +1,31 @@
+""" Test for django admin modifications """
+
+from django.test import TestCase, Client
+from django.contrib.auth import get_user_model
+from django.urls import reverse
+
+
+class AdminSiteTests(TestCase):
+    """ Test for Django admin site """
+
+    def setUp(self):
+        """ Create a test client and an admin user"""
+        self.client = Client()
+        self.admin_user = get_user_model().objects.create_superuser(
+            email="admin@example.com",
+            password="password123"
+        )
+        self.client.force_login(self.admin_user)
+        self.user = get_user_model().objects.create_user(
+            email="user@example.com",
+            password="password123",
+            name="test user"
+        )
+
+    def test_users_list(self):
+        """ Test that users are listed on user page """
+        url = reverse("admin:core_user_changelist")
+        response = self.client.get(url)
+
+        self.assertContains(response, self.user.name)
+        self.assertContains(response, self.user.email)
