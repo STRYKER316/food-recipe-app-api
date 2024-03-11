885644a42cf9230b73bc4af01364af7fa58e2a27
STRYKER316 <ansil.mishra316@gmail.com>
2024-03-12 00:22:24 +0530

    Add recipe app with serializers, views, and URL configuration

---
+++ app/recipe/serializers.py
@@ -0,0 +1,16 @@
+"""
+Serializers for recipe app
+"""
+
+from rest_framework import serializers
+
+from core.models import Recipe
+
+
+class RecipeSerializer(serializers.ModelSerializer):
+    """ Serialzer for recipe objects """
+
+    class Meta:
+        model = Recipe
+        fields = ['id', 'title', 'time_minutes', 'price', 'link']
+        read_only_fields = ['id']
