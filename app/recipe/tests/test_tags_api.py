211d8814fcab913460d6b520292ee3393648cd21
STRYKER316 <ansil.mishra316@gmail.com>
2024-03-12 20:10:38 +0530

    Add unit-test for updating a tag

--- app/recipe/tests/test_tags_api.py
+++ app/recipe/tests/test_tags_api.py
@@ -17,6 +17,11 @@ from recipe.serializers import TagSerializer
 TAGS_URL = reverse('recipe:tag-list')
 
 
+def detail_url(tag_id):
+    """ Create and return tag detail URL """
+    return reverse('recipe:tag-detail', args=[tag_id])
+
+
 def create_sample_user(email='user@example.com', password='testpass'):
     """ Create and return a sample user """
     return get_user_model().objects.create_user(email=email, password=password)
@@ -68,3 +73,15 @@ class PrivateTagsApiTests(TestCase):
         self.assertEqual(len(res.data), 1)
         self.assertEqual(res.data[0]['name'], tag.name)
         self.assertEqual(res.data[0]['id'], tag.id)
+
+    def test_update_tag(self):
+        """ Test updating a tag """
+        tag = Tag.objects.create(user=self.user, name='Post-dinner')
+
+        payload = {'name': 'Dessert'}
+        url = detail_url(tag.id)
+        res = self.client.patch(url, payload)
+
+        self.assertEqual(res.status_code, status.HTTP_200_OK)
+        tag.refresh_from_db()
+        self.assertEqual(tag.name, payload['name'])
