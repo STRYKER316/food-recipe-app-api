4daaf5389fb8a380e82de8b3f6704ea736fb347f
STRYKER316 <ansil.mishra316@gmail.com>
2024-03-13 18:33:35 +0530

    Implement support for creating ingridients with recipe creation

--- app/recipe/serializers.py
+++ app/recipe/serializers.py
@@ -29,10 +29,14 @@ class RecipeSerializer(serializers.ModelSerializer):
     """ Serialzer for recipe objects """
 
     tags = TagSerializer(many=True, required=False)
+    ingredients = IngredientSerializer(many=True, required=False)
 
     class Meta:
         model = Recipe
-        fields = ['id', 'title', 'time_minutes', 'price', 'link', 'tags']
+        fields = [
+            'id', 'title', 'time_minutes', 'price',
+            'link', 'tags', 'ingredients',
+        ]
         read_only_fields = ['id']
 
     def __get_or_create_tags(self, recipe, tags):
@@ -46,12 +50,25 @@ class RecipeSerializer(serializers.ModelSerializer):
             )
             recipe.tags.add(tag_obj)
 
+    def __get_or_create_ingredients(self, recipe, ingredients):
+        """ Handle getting or creating ingredients as needed """
+        auth_user = self.context['request'].user
+
+        for ingredient in ingredients:
+            ingredient_obj, created = Ingredient.objects.get_or_create(
+                user=auth_user,
+                **ingredient,
+            )
+            recipe.ingredients.add(ingredient_obj)
+
     def create(self, validated_data):
         """ Create a recipe """
         tags = validated_data.pop('tags', [])
+        ingredients = validated_data.pop('ingredients', [])
         recipe = Recipe.objects.create(**validated_data)
 
         self.__get_or_create_tags(recipe, tags)
+        self.__get_or_create_ingredients(recipe, ingredients)
 
         return recipe
 
