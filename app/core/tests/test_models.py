643855caccb07ddd83874b302d1189f0747b7f75
STRYKER316 <ansil.mishra316@gmail.com>
2024-03-13 21:36:17 +0530

    Add image field to Recipe model and implement recipe_image_file_path function

--- app/core/tests/test_models.py
+++ app/core/tests/test_models.py
@@ -3,6 +3,7 @@ Tests for models
 """
 from django.test import TestCase
 from django.contrib.auth import get_user_model
+from unittest.mock import patch
 
 from decimal import Decimal
 
@@ -91,3 +92,13 @@ class ModelTests(TestCase):
         )
 
         self.assertEqual(str(ingridient), ingridient.name)
+
+    @patch('core.models.uuid.uuid4')
+    def test_recipe_file_name_uuid(self, mock_uuid):
+        """ Test generating image path """
+        uuid = 'test-uuid'
+        mock_uuid.return_value = uuid
+        file_path = models.recipe_image_file_path(None, 'myimage.jpg')
+
+        expected_path = f'uploads/recipe/{uuid}.jpg'
+        self.assertEqual(file_path, expected_path)
