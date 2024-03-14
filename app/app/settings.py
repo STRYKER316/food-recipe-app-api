c1e425bb3b47897f9183fc0c4ebdf1750aa7b73b
STRYKER316 <ansil.mishra316@gmail.com>
2024-03-15 01:01:47 +0530

    Prepare for Deployment

--- app/app/settings.py
+++ app/app/settings.py
@@ -21,12 +21,18 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-(j0rxs4dcfxjjl4k16ecyr)s3!bwzd&mo75_e5awzf8*0)d#*6'
+SECRET_KEY = os.environ.get('SECRET_KEY', 'changeme')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = bool(int(os.environ.get('DEBUG', 0)))
 
 ALLOWED_HOSTS = []
+ALLOWED_HOSTS.extend(
+    filter(
+        None,
+        os.environ.get('ALLOWED_HOSTS', '').split(','),
+    )
+)
 
 
 # Application definition
--- scripts/run.sh
+++ scripts/run.sh
@@ -2,8 +2,8 @@
 
 set -e
 
-python manage.py wait_fo_db
-python mannage.py collectstatic --noinput
+python manage.py wait_for_db
+python manage.py collectstatic --noinput
 python manage.py migrate
 
 uwsgi --socket :9000 --workers 4 --master --enable-threads --module app.wsgi
